7ef6a47e176a6d7a792a737c49eaf590ae807557
israel-sahar <37867238+israel-sahar@users.noreply.github.com>
2019-06-30 23:30:39 +0300

    .

--- Vacation_dates_confimation/script.js
+++ Vacation_dates_confimation/script.js
@@ -1,125 +1,135 @@
-$(document).ready(function(){
-    localStorage.setItem("carRental","no");
-    localStorage.setItem("hotelStars","5");
-    console.log(localStorage.getItem("carRental"));
-    
+$(document).ready(function () {
+    localStorage.setItem("carRental", "no");
+    localStorage.setItem("hotelStars", "5");
+
     var userID
     var test
     var vacationArr = [];
     firebase.auth().onAuthStateChanged(function (user) {
-        if(user){
-            userID=user.uid
-            test = firebase.database().ref().child('dates/'+user.uid);
-            test.once('value').then(function(snapshot) {
-                if(snapshot.val()==null)
-                {
+        if (user) {
+            $('#userImg').html('<img src="' + user.photoURL + '">')
+            $('#topUserName').text("Hello, " + user.displayName)
+
+            userID = user.uid
+            test = firebase.database().ref().child('dates/' + user.uid);
+            test.once('value').then(function (snapshot) {
+                if (snapshot.val() == null) {
                     $('#noDates').text("You didnt registed dates yet!");
                     return;
-                }  
+                }
                 $('#noDates').text(" ");
-                 
-                var i=0        
-                let howManyPrints = datesCounter; 
-                snapshot.forEach(function(childSnapshot) {
-                   
-                    let childData = childSnapshot.val();
-                    vacationArr[i++]={'cityname':childSnapshot.val()['CityName'],
-                                      'sDate':childSnapshot.val()['sDate'],
-                                      'eDate':childSnapshot.val()['eDate'],
-                                    'carRental': childSnapshot.val()['carRental'],
-                                    'hotelStars': childSnapshot.val()['hotelStars']}
-
-                     $("#dates").append('<li class="list-group-item">' 
-                   + childSnapshot.val()['sDate'] + " - " + childSnapshot.val()['eDate'] + " in  "+ childSnapshot.val()['CityName'] +";  " +"    rent a car? - "+ childSnapshot.val()['carRental'] 
-                    +";    Hotel stars- "+ childSnapshot.val()['hotelStars'] + '</li>');
-                     howManyPrints--;
-                     if(howManyPrints==0){
-                        return true; 
-                     }
-                     
-                 });
-                 
-                 console.log(vacationArr);
+                var i = 0
+                snapshot.forEach(function (childSnapshot) {
+                    vacationArr[i++] = {
+                        'sDate': new Date(childSnapshot.val()['sDate']),
+                        'eDate': new Date(childSnapshot.val()['eDate']),
+                        'hotelStars': childSnapshot.val()['hotelStars'],
+                        'CityName': childSnapshot.val()['CityName'],
+                        'carRental': childSnapshot.val()['carRental']
+                    }
                 });
-        $('#userImg').html('<img src="' + user.photoURL + '">')
-        $('#topUserName').text("Hello, " + user.displayName)
+                console.log(vacationArr)
+                printArray()
 
-
-
-
-        }else{
+            });
+        } else {
             location.href = '../homepage/homepage.html'
         }
-
     });
-    
+    function printArray()
+    {
+        $("#dates").empty()
+        vacationArr.sort(function(a,b){
+            return a['sDate'].getTime()>b['sDate'].getTime()
+        })
+        console.log(vacationArr)
+        vacationArr.forEach(function (unit) {
+            $("#dates").append('<li class="list-group-item">'
+                + unit['sDate'].toLocaleDateString() + " - " + unit['eDate'].toLocaleDateString() + " in  " + unit['CityName'] + ";  " + "    rent a car? - " + unit['carRental']
+                + ";    Hotel stars- " + unit['hotelStars'] + '</li>');
 
+        })
+    }
 
-    $("#logOut").click(function(){
-        firebase.auth().signOut().then(function() {
-            // Sign-out successful.
+    $("#logOut").click(function () {
+        firebase.auth().signOut().then(function () {
             location.href = '../homepage/homepage.html'
-        }).catch(function(error) {
-            // An error happened.
-        });
-      });
+        }).catch(function (error) { });
+    });
 
 
-   $("#YES").click(function() {
-       localStorage.setItem("carRental","yes");
-       console.log(localStorage.getItem("carRental"));
-         
-   })
+    $("#YES").click(function () {
+        localStorage.setItem("carRental", "yes"); })
 
-$("#1star").click(function() {
-    localStorage.setItem("hotelStars","1");  
-})
+    $("#1star").click(function () {
+        localStorage.setItem("hotelStars", "1");
+    })
 
-$("#2star").click(function() {
-    localStorage.setItem("hotelStars","2");  
-})
-$("#3star").click(function() {
-    localStorage.setItem("hotelStars","3");  
-})
-$("#4star").click(function() {
-    localStorage.setItem("hotelStars","4");  
-})
+    $("#2star").click(function () {
+        localStorage.setItem("hotelStars", "2");
+    })
+    $("#3star").click(function () {
+        localStorage.setItem("hotelStars", "3");
+    })
+    $("#4star").click(function () {
+        localStorage.setItem("hotelStars", "4");
+    })
 
-
-
-   
-  
-      var datesCounter=0;       //for showing the correct amount of dates in the list
-
-    $("#saveDate").click(function(){
+    $("#saveDate").click(function () {
         $('#FaildMsg').text("")
-        s=new Date($("#startDate").val())
-        e=new Date($("#endDate").val())
-        if(s=="Invalid Date"||e=="Invalid Date"){
+        s = new Date($("#startDate").val())
+        e = new Date($("#endDate").val())
+        if (s == "Invalid Date" || e == "Invalid Date") {
             $('#FaildMsg').text("Erorr! please enter a valid date")
             return;
         }
-        else{
-            if(s>e){
+        else {
+            if (s > e) {
                 $('#FaildMsg').text("Erorr! please enter a valid date")
-                return;  
+                return;
             }
-            else{
-               
+            else {
+                var newDate = { 'sDate': s, 
+                                'eDate': e,
+                                'hotelStars': localStorage.getItem("hotelStars"),
+                                'CityName':localStorage.getItem("selectedCity"),
+                                'carRental':localStorage.getItem("carRental")}
+                if (!checkDates(newDate)) {
+                    $('#FaildMsg').text("Erorr! please enter a valid date")
+                    return;
+                }
+                vacationArr[vacationArr.length] = newDate;
                 var node = test.push();
-                datesCounter++;
                 node.set({
-                CityName: localStorage.getItem("selectedCity"),
-                eDate: e.toLocaleDateString(),
-                sDate: s.toLocaleDateString(),
-                carRental: localStorage.getItem("carRental"),
-                hotelStars: localStorage.getItem("hotelStars")
+                    CityName: localStorage.getItem("selectedCity"),
+                    eDate: e.toLocaleDateString(),
+                    sDate: s.toLocaleDateString(),
+                    carRental: localStorage.getItem("carRental"),
+                    hotelStars: localStorage.getItem("hotelStars")
 
-            });
-               
-
+                });
+                printArray()
             }
-                    
         }
-    });
+    }
+    );
+
+    function checkDates(newDate) {
+        if(vacationArr.length==0) return true;
+        for (var i = 0; i < vacationArr.length; i++) {
+            if(i==0){
+                if(newDate['eDate']<vacationArr[i]['sDate']) return true;
+            }
+            if (i + 1 == vacationArr.length) {
+                console.log()
+                if (vacationArr[i]['eDate'] < newDate['sDate']) return true;
+            }
+            else {
+                if (newDate['sDate'] > vacationArr[i]['eDate']&& newDate['eDate'] < vacationArr[i + 1]['sDate'])
+                    return true;
+            }
+
+        }
+        return false;
+    }
 })
